0b74d6ff51fbc817b653c5f6f9e9cb96d6da7341
Bertrand Caron <bertrand.caron@bnf.fr>
2020-05-10 16:22:47 +0200

    Version du fichier pour le TP 2
    Adaptation de la proposition de Thomas à ma sauce pour le rendre cohérent avec le texte du TP 2.

--- ark_2.py
+++ ark_2.py
@@ -1,47 +1,51 @@
-"""Module de gestion des ark"""
+"""Module de gestion des chaînes de caractères ARK BnF"""
+
+
 import sys
 
 
-class ChaineArkBnf():
-    """Classe de gestion des identifiants ARK BnF"""
+ALPHABET = '0123456789bcdfghjkmnpqrstvwxz'
 
-    ALPHABET = '0123456789bcdfghjkmnpqrstvwxz'
 
-    @staticmethod
-    def trouver_indice(carac):
-        """Trouve l'indice du caractère dans l'alphabet à 29 lettres"""
-        if len(carac) != 1:
-            msg = "Il faut fournir un seul caractère :{}".format(carac)
-            raise ValueError(msg)
-        try:
-            return ChaineArkBnf.ALPHABET.index(carac)
-        except ValueError:
-            msg = "{} n'est pas un caractère de l'alphabet ARK BnF valide"
-            raise ValueError(msg.format(carac))
+class ChaineARKBnF():
+    """Classe principale de gestion des chaînes ARK BnF"""
 
     def __init__(self, valeur=''):
-        """Constructeur"""
+        """Constructeur
+
+        Il définit l'attribut unique d'une chaîne de
+        caractères ARK BnF : sa valeur."""
         self.valeur = valeur
 
     def valider(self):
-        """Valide les caractères contenus dans la chaîne ark"""
+        """Méthode de validation d'une chaîne
+
+        Elle renvoie un tuple avec en première valeur un booléen et en
+        seconde un message qui spécifie le cas échéant les caractères
+        invalides."""
         invalides = []
         for carac in self.valeur:
-            if carac not in ChaineArkBnf.ALPHABET:
+            if carac not in ALPHABET:
                 invalides.append(carac)
-        if len(invalides) == 0:
-            return (True, "La chaîne est valide au regard de l'alphabet BnF.")
-        msg = ("La chaîne n'est pas valide,"
-               "les caractères {} ne figurent pas dans l'alphabet BnF.")
-        return (False, msg.format(set(invalides)))
+        if not invalides:
+            valide = True
+            msg = "La chaîne est valide au regard de l'alphabet BnF."
+        else:
+            valide = False
+            msg = ("La chaîne n'est pas valide, le(s) caractère(s) {} "
+                   "ne figurent pas dans l'alphabet BnF.").format(set(invalides))
+        return (valide, msg)
 
     def calculer_controle(self):
-        """Calcule le caractère de contrôle associé à la chaîne ark"""
+        """Méthode de calcul du caractère de contrôle
+
+        Elle renvoie le caractère de contrôle si la chaîne est valide et
+        gère les erreurs."""
         try:
             addition = 0
-            for (i, carac) in enumerate(self.valeur):
-                addition += ChaineArkBnf.ALPHABET.index(carac) * (i + 1)
-            controle = ChaineArkBnf.ALPHABET[addition % 29]
+            for indice, carac in enumerate(self.valeur):
+                addition += ALPHABET.index(carac) * (indice+1)
+            controle = ALPHABET[addition % 29]
             return controle
         except ValueError:
             valide = self.valider()
@@ -51,12 +55,16 @@ class ChaineArkBnf():
 
 
 def main(args):
-    """Fonction principale"""
-    value = args[1] if len(args) > 1 else "t3st"
-    ark = ChaineArkBnf(value)
+    """Fonction principale
+
+    Elle calcule le caractère de contrôle de toute chaîne ARK BNF passée en
+    argument."""
+    valeur = args[1] if len(args) > 1 else "t3st"
+    chaine = ChaineARKBnF(valeur)
+    controle = chaine.calculer_controle()
     msg = "Pour {}, le caractère de contrôle est {}"
-    print(msg.format(value, ark.calculer_controle()))
+    print(msg.format(valeur, controle))
+
 
 if __name__ == '__main__':
     main(sys.argv)
-
